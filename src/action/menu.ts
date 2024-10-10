8b8c3eca3303bbdd4fcc30f1207bb52b30236edd
Lwei <251923281@qq.com>
2024-10-10 18:48:45 +0800

    fix

--- src/action/menu.ts
+++ src/action/menu.ts
@@ -3,8 +3,10 @@ import message from "../trigger/message";
 export default class {
     constructor(cls:message){
         cls.addLine('┏┄══✉️菜单列表══━┄')
+        cls.addLine('┃ [活动]查看当前进行的活动')
         cls.addLine('┃ [位置]查看当前位置')
-        cls.addLine('┃ [all]艾特全体测试')
+        cls.addLine('┃ [任务]超多任务等你参与')
+        cls.addLine('┃ [我的]与我相关')
         cls.addLine('┃ V1.0.2')
         cls.addLine('┗━━━━━━━━━━━━┄')
         cls.send()
