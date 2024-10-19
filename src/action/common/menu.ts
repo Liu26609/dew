4ba32245b33a4346d046f62e098c1f620986f6db
匿名用户 <8797550+chuxuand30@user.noreply.gitee.com>
2024-10-19 21:23:25 +0800

    指令整理

--- src/action/common/menu.ts
+++ src/action/common/menu.ts
@@ -3,12 +3,11 @@ import message from "../../trigger/message"
 export default class {
     constructor(cls:message){
         cls.addLine('┏┄══✉️基础指令══━┄')
-        cls.addLine('┃ [探索]查看当前进行的活动')
-        cls.addLine('┃ [位置]查看当前位置')
-        cls.addLine('┃ [战斗]超多任务等你参与')
+        cls.addLine('[角色]查看自身相关的指令')
+        cls.addLine('[世界]查看世界相关指令')
         cls.addLine('┃════✉️提示系统═━┄')
-        cls.addLine('┃ Tips[指令 + hp]可查看指令的描述')
-        cls.addLine('┃ 🌰栗子：探索hp')
+        cls.addLine('[指令 + hp]可查看指令的描述')
+        cls.addLine('🌰栗子：探索hp')
         cls.addLine('┗━━━━━━━━━━━━┄')
         cls.send()
     }
--- src/cfg/actionCfg.ts
+++ src/cfg/actionCfg.ts
@@ -13,44 +13,49 @@ interface _data {
     path:string
 }
 let actionCfg:_data[] = [];
-// 活动相关
 actionCfg.push({
-    // active/签到
-    key: "签到",
+    key: "日常活动/签到",
     key_tips:'每日一签',
     tips: "签到天数越多奖励越多\n每日签到排名越高奖励越多",
     example: ["@机器人 签到"],
     path: "active/签到"
 })
-// // battle
 actionCfg.push({
-    key: "逃跑",
+    key: "世界/逃跑",
     key_tips:'保命要紧，战术撤退',
     tips: "",
     example: ["@机器人 逃跑"],
     path: "battle/逃跑"
 })
 actionCfg.push({
-    key: "探索",
+    key: "世界/探索",
     key_tips:'前面的蛆以后再探索叭',
     tips: "",
     example: ["@机器人 探索"],
     path: "player/map/探索"
 })
 actionCfg.push({
-    key: "开始历练 <世界名:string>",
-    key_tips:'开始历练',
+    key: "世界/历练 <世界名:string>",
+    key_tips:'进入轮回世界历练变强',
     tips: "",
     example: ["@机器人 开始历练","@机器人 开始历练 火影忍者"],
     path: "player/map/开始历练"
 })
 actionCfg.push({
-    key: "战斗",
+    key: "世界/战斗",
     key_tips:'战斗！爽！',
     tips: "立刻开始即将发生的战斗",
     example: ["@机器人 战斗"],
     path: "battle/战斗"
 })
+actionCfg.push({
+    key: "世界/位置",
+    key_tips:'查看当前世界信息',
+    tips: "位置",
+    example: ["@机器人 位置"],
+    path: 'player/info/位置'
+})
+
 
 actionCfg.push({
     key: "debug <调试路径:string>",
@@ -67,31 +72,25 @@ actionCfg.push({
     path: 'common/menu'
 })
 actionCfg.push({
-    key: "sys",
-    key_tips:'sys',
+    key: "sys.userinfo",
+    key_tips:'展示用户id',
     tips: "sys",
     example: ["@机器人 sys"],
     path: 'sys/info'
 })
 actionCfg.push({
-    key: "改名",
-    key_tips:'改名',
+    key: "角色/改名",
+    key_tips:'角色改名',
     tips: "改名",
     example: ["@机器人 改名"],
     path: 'player/info/改名'
 })
 actionCfg.push({
-    key: "属性",
-    key_tips:'属性',
+    key: "角色/属性",
+    key_tips:'查看我的属性',
     tips: "属性",
     example: ["@机器人 属性"],
     path: 'player/info/属性'
 })
-actionCfg.push({
-    key: "位置",
-    key_tips:'位置',
-    tips: "位置",
-    example: ["@机器人 位置"],
-    path: 'player/info/位置'
-})
+
 export default actionCfg;
