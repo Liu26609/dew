b48ef4c27ba5feaf09a76a979007bf3cc7c4110e
匿名用户 <8797550+chuxuand30@user.noreply.gitee.com>
2024-10-20 12:43:12 +0800

    test

--- dist/action/debug/atAll.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class default_1 {
-    constructor(cls) {
-        cls.addLine('@everyone');
-        cls.send();
-    }
-}
-exports.default = default_1;
