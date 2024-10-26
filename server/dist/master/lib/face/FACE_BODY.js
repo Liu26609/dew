c023dbaee9ec3923e474553659afa69a508f53fc
匿名用户 <8797550+chuxuand30@user.noreply.gitee.com>
2024-10-26 21:36:32 +0800

    rm

--- server/dist/master/lib/face/FACE_BODY.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.battle_group = void 0;
-var battle_group;
-(function (battle_group) {
-    battle_group[battle_group["\u4E3B\u573A"] = 0] = "\u4E3B\u573A";
-    battle_group[battle_group["\u5BA2\u573A"] = 1] = "\u5BA2\u573A";
-})(battle_group = exports.battle_group || (exports.battle_group = {}));
--- server/dist/shared/master/transaction/PtlCancel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.conf = void 0;
-exports.conf = {
-    check_onlyid: true
-};
