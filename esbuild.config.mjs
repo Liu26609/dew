ec6932b6cf80c935f946211260d115dbf2ecfb75
匿名用户 <8797550+chuxuand30@user.noreply.gitee.com>
2024-11-02 19:29:07 +0800

    build test

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,7 +1,7 @@
 import esbuild from 'esbuild'
 import { copy } from 'esbuild-plugin-copy';
 const options = {
-    entryPoints: ['./src/**/*.ts'], // 使用通配符指定多个入口点
+    entryPoints: ['./src/index.ts'], // 使用通配符指定多个入口点
     bundle: true,
     outdir: 'lib',
     outbase: './src', // 保留目录结构
@@ -14,7 +14,7 @@ const options = {
             // 复制静态文件并保留目录结构
             assets: {
                 from: ['./src/**/*.html', './src/**/*.css', './src/**/*.ttf'],
-                to: ['./lib'],
+                to: ['./html'],
                 keepStructure: true, // 保留目录结构
             },
         }),
