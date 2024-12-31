edae662defddec3481dab70d56785bbdb6f49204
Lwei <251923281@qq.com>
2024-12-31 16:11:25 +0800

    预警bug

--- src/action/work/后台日志/预警任务.ts
+++ src/action/work/后台日志/预警任务.ts
@@ -5,46 +5,65 @@ import temp_img from "../../../temp/temp_img";
 import { CFG, log } from "../../..";
 let auto = false;
 export default class {
+    // 定时
+    scheduleTask: any;
+    lastSendTime: number = 0;
     constructor(cls: message, ...data) {
         console.log('绑定信息', data)
-        this.start(cls, false)
-        if(!auto){
-            auto = true;
-            const executeTask = async () => {
-                await this.start(cls, true);
-            };
-
-            const scheduleTask = () => {
-                const now = new Date();
-                const minutes = now.getMinutes();
-                const seconds = now.getSeconds();
-                const milliseconds = now.getMilliseconds();
-                const delay = (CFG.预警定时 - (minutes % CFG.预警定时)) * 60 * 1000 - seconds * 1000 - milliseconds;
-
-                setTimeout(() => {
-                    log.info('[预警任务]', '开始执行预警任务',APP.follow_list.size);
-                    executeTask();
-                    setInterval(executeTask, 1000 * 60 * CFG.预警定时);
-                }, delay);
-            };
-
-            scheduleTask();
+        if (data.length == 0) {
+            this.start(cls, false)
+        } else if (data.length == 1) {
+            this.open_task(cls, data[0])
         }
     }
+    open_task(cls: message, type: string) {
+        if (type !== '开启') {
+            return;
+        }
+        if (this.scheduleTask) {
+            // 移除
+            clearTimeout(this.scheduleTask);
+            clearInterval(this.scheduleTask);
+            this.scheduleTask = null;
+        }
+        
+        const executeTask = async () => {
+            console.log('执行预警任务 ----- ')
+            await this.start(cls, true);
+        };
 
+        const now = new Date();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+        const milliseconds = now.getMilliseconds();
+        const delay = ((CFG.预警定时 - (minutes % CFG.预警定时)) * 60 * 1000) - (seconds * 1000) - milliseconds;
+
+        cls.send_v1(`定时:${CFG.预警定时}分钟,${(delay / 1000 / 60).toFixed(2)}分钟后开始通知`);
+
+        this.scheduleTask = setTimeout(() => {
+            log.info('[预警任务]', '开始执行预警任务', APP.follow_list.size);
+            executeTask();
+            this.scheduleTask = setInterval(executeTask, 1000 * 60 * CFG.预警定时);
+        }, delay);
+
+    }
     async start(cls: message, auto) {
-        console.log(cls);
+        if(Date.now() - this.lastSendTime < 100){
+            console.log('预警任务太频繁')
+            return;
+        }
+        this.lastSendTime = Date.now();
         let img = await temp_img.render_url(cls);
         let str = ``;
-        let res =  await this.check_1();
+        let res = await this.check_1();
         str += `${res}`;
         str += await this.check_2();
         str += await this.check_3();
         str += img;
-        if(!auto){
+        if (!auto) {
             let 是否订阅 = APP.follow_list.has(cls.get_userId());
             cls.send_v1(`${str}<button text="预警检查" type="input">再来一次</button><button text="${是否订阅 ? '取消订阅' : '订阅预警'}" type="input">${是否订阅 ? '取消订阅' : '订阅预警'}</button>`)
-        }else{
+        } else {
             APP.follow_list.forEach((value, key) => {
                 value.send_v1(`${str}<button text="预警检查" type="input">再来一次</button>`)
             });
@@ -93,7 +112,7 @@ export default class {
         } else {
             str += '🟢'
         }
-        str += `近30分钟内充值总笔数${halfHourCount},充值成功率${(rate*100).toFixed(2)}%,充值成功金额${success_amount}`;
+        str += `近30分钟内充值总笔数${halfHourCount},充值成功率${(rate * 100).toFixed(2)}%,充值成功金额${success_amount}`;
         str += `</p>`;
         return str
     }
@@ -113,15 +132,15 @@ export default class {
         for (let i = 0; i < res.data.list.length; i++) {
             if (res.data.list[i].status == 1) {
                 count++;
-                if(res.data.list[i].souAmount){
+                if (res.data.list[i].souAmount) {
                     wait_count += res.data.list[i].souAmount / 100;
                 }
             }
-            
+
         }
-        if(count > 0){
+        if (count > 0) {
             return `<p>🔴近100次提现待审核数量:${count}${wait_count > 0 ? `,待审核金额:${wait_count}元` : ''}</p>`
-        }else{
+        } else {
             return `<p>🟢近100次提现无待审核</p>`
         }
     }
@@ -140,9 +159,9 @@ export default class {
             }
         }
         let rate = count / res.data.list.length;
-        if(rate > 0.5){
+        if (rate > 0.5) {
             return `<p>🟢近30条验证码使用率:${(rate * 100).toFixed(2)}%</p>`
-        }else{
+        } else {
             return `<p>🔴近30条验证码使用率:${(rate * 100).toFixed(2)}%</p>`
         }
     }
