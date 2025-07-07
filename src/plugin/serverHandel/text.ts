9e72cf619116238c5c3da9e6078eea6f82a678b8
Lwei <251923281@qq.com>
2025-07-07 15:03:33 +0800

    简化文本消息处理逻辑，移除对消息类型的判断，直接发送数据。

--- src/plugin/serverHandel/text.ts
+++ src/plugin/serverHandel/text.ts
@@ -1,15 +1,11 @@
 import handel_base from "./handel_base"
 
-export default class handelr_text extends handel_base  {
+export default class handelr_text extends handel_base {
     constructor(data: any) {
         super(data)
     }
     async start(data: any) {
-        if(data.info.type == 'text'){
-            await this.send(data, data.data)
-        }else{
-            console.log('未知类型的文本消息')
-        }
+        await this.send(data, data.data)
     }
 }
 
